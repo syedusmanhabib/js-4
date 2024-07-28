b90ae2fbcbd24ede42de811631cad13df4ba95a7
syedusmanhabib <126954761+syedusmanhabib@users.noreply.github.com>
2024-07-28 11:16:53 +0000

    rgfgrtfhtth

---
+++ maani4.js
@@ -0,0 +1,34 @@
+// primittive data Type    
+
+// string boolean bigint number null undefined symbol 
+
+const score = 100
+const scorevalue = 100.5
+const isloggedIn = false
+const outsideTemp =  null
+let useremail;
+
+const id = Symbol ('456')
+const anotherID = Symbol ('456')
+console.log(id===anotherID);
+
+// const bigNumber = 454545454545444n
+
+
+// non primittive data types 
+
+// arrays object functions
+
+// const usmans = ["zuraiz", "zain", "sheharyar"];
+// let myobj = {
+//     name: "maani",
+//     age : 20,
+//     department : cs
+// }
+
+// const myFunction = function(){
+//     console.log("usman")
+
+// }
+// console.log(typeof bigNumber);
+// console.log(typeof Symbol);
